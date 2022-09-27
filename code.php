2d7a1311577bcf25f2a3ae9d71a902f702b31cb9
Thomas Aldaitz <thomas.aldaitz@wildcodeschool.com>
2022-09-27 11:26:28 +0200

    Add the very easy correction

--- code.php
+++ code.php
@@ -2,4 +2,23 @@
 
 function transform($start, $end) {
 
+    if ($start < 0 || $end < 0) return "Les paramètres doivent etre positifs";
+    if ($start > $end) return "La fin doit est supérieur au début";
+    if ($end > 9) return "La fin ne peut pas dépasser 9";
+
+    /* solution 1
+    $result = "";
+    for ($i = $start; $i < $end; $i++)
+    {
+        $result = $result . $i . '-';
+    }
+
+    $result .= $end;
+
+    return $result;
+
+    */
+
+    $range = range($start, $end);
+    return implode('-', $range);
 }
